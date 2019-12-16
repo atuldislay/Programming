ac9a5e59019c7561caf02eaf1fe9da4459bce9e6
Atul Dislay <atuldislay@gmail.com>
2019-12-16 15:58:53 +0530

    Get Random Node from Linked List
    Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
    
    Follow up:
    What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
    
    Example:
    
    // Init a singly linked list [1,2,3].
    ListNode head = new ListNode(1);
    head.next = new ListNode(2);
    head.next.next = new ListNode(3);
    Solution solution = new Solution(head);
    
    // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
    solution.getRandom();

---
+++ RandomNodeLinkedList.cs
@@ -0,0 +1,64 @@
+ public class RandomNodeLinkedList
+    {
+        private Node _head;
+
+        public class Node
+        {
+            public readonly int Data;
+            public Node Next;
+
+            public Node(int d)
+            {
+                Data = d;
+                Next = null;
+            }
+        }
+
+        public void AddAtStart(int data)
+        {
+            Node node = new Node(data);
+            if (_head == null)
+            {
+                _head = node;
+                return;
+            }
+
+            node.Next = _head;
+            _head = node;
+        }
+
+        public void PrintList()
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            Node temp = _head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Data);
+                temp = temp.Next;
+            }
+        }
+
+        public int GetRamdom()
+        {
+
+            int restult = _head.Data;
+            Node current = _head;
+            for (int n = 2; current != null; n++)
+            {
+
+                if (new Random().Next() % n == 0)
+                {
+                    restult = current.Data;
+                }
+
+                current = current.Next;
+            }
+
+            return restult;
+
+        }
+    }
