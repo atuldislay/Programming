da4ef654cf44c80dfd5fe623ac4c0ccfbf7d6342
Atul Dislay <atuldislay@gmail.com>
2019-12-28 14:06:35 +0530

    82. Remove Duplicates from Sorted List II
    Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
    
    Example 1:
    
    Input: 1->2->3->3->4->4->5
    Output: 1->2->5
    Example 2:
    
    Input: 1->1->1->2->3
    Output: 2->3

---
+++ DeleteAllDuplicate.cs
@@ -0,0 +1,26 @@
+public ListNode DeleteDuplicates(ListNode head) {
+         ListNode dummy = new ListNode(0);
+
+			dummy.next = head;
+			ListNode prev = dummy;
+			ListNode current = head;
+
+			while (current != null)
+			{
+				while (current.next != null &&
+					prev.next.val == current.next.val)
+					current = current.next;
+
+				if (prev.next == current)
+					prev = prev.next;
+
+				else
+					prev.next = current.next;
+
+				current = current.next;
+			}
+
+			head = dummy.next;
+        return head;
+
+    }
